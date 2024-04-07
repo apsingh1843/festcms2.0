7c8951da0a78f4e0457b46c2de32b7d369a80089
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2024-04-07 23:03:22 +0530

    Home Page and Teams Page Added

--- flow/static/js/home.js
+++ flow/static/js/home.js
@@ -1,41 +1,60 @@
-const VIDEO = document.getElementById('video');
-const offsetCloseBtnHidden = document.getElementById('hiddenBtn');
-const offsetShowBtnHidden = document.getElementById('shownBtn');
+const windowInnerWidth = window.innerWidth;
+const windowInnerHeight = window.innerHeight;
+console.log(windowInnerHeight);
+console.log(windowInnerWidth);
 
-// VIDEO.onloadeddata = function() {
-//     console.log("Video loaded");
-//     setTimeout(() => {
-//         loadend();
-//     }, 2000);
-// };
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        console.log("loaded");
-        setTimeout(() => {
-            loadend();
-        }, 2000);
-    } else {
-        console.log("not loaded");
-        setTimeout(() => {
-            loadend();
-        }, 2000);
-    }
+var videoSources = {
+    desktop: '/static/videos/flp.mp4',
+    phone: '/static/videos/fph.mp4',
+    blink: '/static/videos/fblink.mp4'
 };
 
-function closeAfterSometimePC() {
-    document.getElementById('pcham').classList.toggle('active')
-    setTimeout(() => {
-        offsetCloseBtnHidden.click()
-        document.getElementById('pcham').classList.toggle('active')
-    }, 800);
-}
-function closeAfterSometimeMob(){
-    document.getElementById('ocanva').classList.toggle('active')
-    // setTimeout(() => {
-        offsetShowBtnHidden.click()
-        // }, 650);
-}
-function loadend() {
-    document.getElementById('loading').style.display= "none";
-    document.getElementById('root').style.display= "block";
-}
+var audioSources = {
+    audio: '/static/videos/bau.mp3'
+};
+window.addEventListener('DOMContentLoaded', function () {
+    var videoElement = document.getElementById('dynamic_video');
+    var audioElement = document.getElementById('audio');
+    // var videoElement2 = document.getElementById('dynamic_video2');
+
+
+    function setVideoSource() {
+        var sourceURL;
+        if (windowInnerWidth <= 1000) {
+            sourceURL = videoSources.phone;
+            videoElement.src = sourceURL;
+            videoElement.loop = true;
+
+        } else {
+            sourceURL = videoSources.desktop;
+            videoElement.src = sourceURL;
+
+            videoElement.addEventListener('ended', function () {
+                sourceURL = videoSources.blink;
+                videoElement.src = sourceURL;
+                videoElement.loop = true;
+                // videoElement.currentTime = 0;
+                // videoElement.play()
+            });
+        }
+
+
+        audioElement.autoplay = true;
+        audioElement.loop = true;
+
+        videoElement.autoplay = true;
+        videoElement.muted = true;
+
+    }
+
+    setVideoSource();
+
+    let test_sound = new Audio('audio');
+    test_sound.play();
+    test_sound.controls();
+    console.log(test_sound);
+
+
+    window.addEventListener('load', setVideoSource);
+    window.addEventListener('resize', setVideoSource);
+});
--- flow/static/js/teams.js
+++ flow/static/js/teams.js
@@ -1,128 +1,60 @@
-var category=0
+/* CODE FOR CARD */
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
 
-colorPalette=["#0a043c","#03506f","#4cd3c2","#b7efcd"]
+    cards.forEach(card => {
+        card.addEventListener('click', function () {
+            const hamburgerMenu = document.querySelector("#hamburger-menu");
+            hamburgerMenu.classList.add('hide-transition');
+            setTimeout(() => {
+                hamburgerMenu.classList.add('hidden');
+            }, 500);
 
-teams=JSON.parse(document.getElementById("teams").textContent)
-members=JSON.parse(document.getElementById("members").textContent)
-console.log(teams)
-console.log(members)
+            setTimeout(() => {
+                const crossInput = document.querySelector('input#button');
+                crossInput.checked = true;
+                hamburgerMenu.classList.remove('hidden');
+                hamburgerMenu.classList.remove('hide-transition');
+            }, 1000);
+        });
+    });
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
 
-var no_of_categories=teams.length;
+    cards.forEach(card => {
+        card.addEventListener('mouseover', mouseOver);
+        card.addEventListener('mouseleave', mouseLeave);
+        card.addEventListener('mousemove', mouseMove);
+    });
+});
 
-var left=document.getElementsByClassName('fa-backward')[0]
-var right=document.getElementsByClassName('fa-forward')[0]
-
-const changeTeams=()=>{
-    cat=category
-    console.log("category idx", category)
-    var no_of_cards=members[category].length;
-
-    // console.log(category,"category")
-    // console.log("changing category",teams[category].name,no_of_cards,members[category][0])
-
-    document.getElementById("cards").innerHTML="";
-
-    for(let i=1;i<=no_of_cards;i++)
-    {
-        document.getElementById("cards").innerHTML+=
-        `<div class="card glitch" id="card${i}" card_no="${i}">
-            <img src="static/aarhnLogo.png" alt="">
-            <div class="glitch__layers">
-                <div class="glitch__layer"></div>
-                <div class="glitch__layer"></div>
-                <div class="glitch__layer"></div>
-            </div>
-            <h1 class="member-details name">Name</h1>
-            <h3 class="member-details name">Club Name</h3>
-        </div>`
-    }
-
-    for(let j=0;j<no_of_cards;j++)
-    {
-        const image=`media\/${members[category][j].profile_img}`
-        document.querySelector(`#card${j+1} img`).src=image;
-        document.querySelectorAll(`#card${j+1} div.glitch__layers div.glitch__layer`).forEach((layer)=>layer.style.backgroundImage=`url(${image})`)
-    }
-
-    const cards=document.querySelectorAll(".card img")
-
-    document.querySelectorAll(".card").forEach((card)=>{
-        let memberDetails=document.getElementById("member-details")
-        let teamCategory=document.getElementById("team-category")
-        
-        card.addEventListener("mouseover",()=>{
-            // card.classList.add("sparkle")
-            card.style.zIndex=2
-
-            let memberName=document.getElementById("member-name")
-            let memberClub=document.getElementById("member-club")
-            let memberPosition=document.getElementById("member-position")
-
-            console.log(category,"cat now")
-
-            let card_no=card.getAttribute("card_no")
-           
-            console.log("card no", members[cat][card_no-1].name)
-            let name= `${members[cat][card_no-1].name}`
-            let club= `${members[cat][card_no-1].choice}`
-            let position= `${members[cat][card_no-1].position}`
-
-            memberName.innerHTML=name
-            memberClub.innerHTML=club
-            memberPosition.innerHTML=position
-
-            memberName.setAttribute("data-text",name)
-            memberPosition.setAttribute("data-text",position)
-            memberClub.setAttribute("data-text",club)
-
-            memberDetails.style.display="flex"
-            teamCategory.style.display="none"
-        })
-        card.addEventListener("mouseleave",()=>{
-            // card.classList.remove("sparkle")
-            card.style.zIndex=0;
-
-            memberDetails.style.display="none"
-            teamCategory.style.display="flex"
-        })
-    })
-        
-    let teamCategory=`${teams[category].name}`
-    let categoryTag=document.getElementById("team-category")
-    categoryTag.innerHTML=teamCategory
-
-    document.getElementById("team-category").setAttribute("data-text",teamCategory)
-    
-    document.getElementById("color-css").href=`../static/css/teams/teams${category%3+1}.css`
+function mouseOver(event) {
+    const cardContent = event.currentTarget.querySelector('.card-content');
+    cardContent.classList.add('hover');
 }
 
-changeTeams()
-
-const changeColor=()=>{
-    category++;
-    if(category==no_of_categories)
-        category=0
-    console.log("changing color")
-    changeTeams()
+function mouseLeave(event) {
+    const cardContent = event.currentTarget.querySelector('.card-content');
+    cardContent.classList.remove('hover');
+    cardContent.style.transform = "";
 }
 
-var interval= setInterval(changeColor,10000)
+function mouseMove(event) {
+    const cardContent = event.currentTarget.querySelector('.card-content');
+    const cardWidth = event.currentTarget.offsetWidth;
+    const cardHeight = event.currentTarget.offsetHeight;
+    const offsetX = event.offsetX;
+    const offsetY = event.offsetY;
 
-left.addEventListener("click", ()=>{
-    clearInterval(interval)
-    interval = setInterval(changeColor,10000)
-    category--;
-    if(category<0)
-    category=no_of_categories-1
-    changeTeams()
-})
+    const rotationY = (offsetX - cardWidth / 2) / 30;
+    const rotationX = (cardHeight / 2 - offsetY) / 35;
 
-right.addEventListener("click", ()=>{
-    clearInterval(interval)
-    interval = setInterval(changeColor,10000)
-    category++;
-    if(category==no_of_categories)
-    category=0
-    changeTeams()
-})
+    if (offsetX > (cardWidth / 2)) {
+        cardContent.style.transform = `translate3d(0px, 0px, 30px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    } else if (offsetX < (cardWidth / 2)) {
+        cardContent.style.transform = `translate3d(0px, 0px, 30px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    }
+}
+/* CODE FOR CARD */
